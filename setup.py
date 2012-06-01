8b9d789ccc5ed1840e8a7173c304c86f954a5447
Brian Waldon <bcwaldon@gmail.com>
2012-06-01 13:57:20 -0700

    Set reasonable version and add description

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 
 setuptools.setup(
     name='warlock',
-    version='0.1',
-    description='',
+    version='0.0.1',
+    description='Python object model built on top of JSON schema',
     author='Brian Waldon',
     author_email='bcwaldon@gmail.com',
     url='http://github.com/bcwaldon/warlock',
