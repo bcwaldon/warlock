cd6467fe76502732dbd98232b138af6ce036652e
Brian Waldon <bcwaldon@gmail.com>
2012-06-01 13:51:26 -0700

    Project bootstrapping

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name='warlock',
+    version='0.1',
+    description='',
+    author='Brian Waldon',
+    author_email='bcwaldon@gmail.com',
+    url='http://github.com/bcwaldon/warlock',
+    packages=['warlock'],
+    install_requires=['jsonschema'],
+)
