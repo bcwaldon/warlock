02833d7c24e9f26d982e2412bbee33aa55b423f1
Brian Waldon <bcwaldon@gmail.com>
2012-07-13 17:46:34 -0700

    Bump version to v0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def parse_requirements():
 
 setuptools.setup(
     name='warlock',
-    version='0.0.1',
+    version='0.0.2',
     description='Python object model built on top of JSON schema',
     author='Brian Waldon',
     author_email='bcwaldon@gmail.com',
