cc61b31265a6eaf5a63ebfe807cbf6a29c35358a
Jan Willhaus <mail@janwillhaus.de>
2019-05-20 12:07:50 +0200

    Bump version, add changelog for 1.3.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setuptools.setup(
     name="warlock",
-    version="1.3.2",
+    version="1.3.3",
     description="Python object model built on JSON schema and JSON patch.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
